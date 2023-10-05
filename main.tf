d573101834a6e173e38ffee4e349ab2744b940d1
Meriam123@*** <reffass.meriam@gmail.com>
2023-10-05 09:32:14 +0000

    Deploying to cloud run

--- main.tf
+++ main.tf
@@ -33,6 +33,11 @@ resource "google_project_service" "resourcemanager" {
   service = "cloudresourcemanager.googleapis.com"
 }
 
+resource "google_project_service" "cloudrun" {
+        project = "devops-tp2"
+  service = "run.googleapis.com"
+}
+
 resource "google_artifact_registry_repository" "artifact" {
   repository_id = "website-tool"
   format = "DOCKER"
@@ -46,6 +51,47 @@ resource "google_sql_user" "wordpress" {
    password = "ilovedevops"
 }
 
+resource "google_cloud_run_service" "default" {
+name     = "serveur-wordpress"
+location = "us-central1"
 
+template {
+   spec {
+      containers {
+      image = "us-central1-docker.pkg.dev/devops-tp2/website-tool/wordpressimag"
+      ports {
+          container_port = 80
+        }
+      }
+   }
 
+   metadata {
+      annotations = {
+            "run.googleapis.com/cloudsql-instances" = "devops-tp2:us-central1:main-instance"
+      }
+   }
+}
+
+traffic {
+   percent         = 100
+   latest_revision = true
+}
+}
+
+data "google_iam_policy" "noauth" {
+   binding {
+      role = "roles/run.invoker"
+      members = [
+         "allUsers",
+      ]
+   }
+}
+
+resource "google_cloud_run_service_iam_policy" "noauth" {
+   location    = google_cloud_run_service.default.location
+   project     = google_cloud_run_service.default.project
+   service     = google_cloud_run_service.default.name
+
+   policy_data = data.google_iam_policy.noauth.policy_data
+}
 
